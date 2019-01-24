c3a3c1fa6854e368757e87ade1798dfd378bf92c
Бойко Сергей <helvisbox@gmail.com>
2019-01-24 18:13:01 +0200

    Added phpunit testing modules;

--- vagrant/provision/once-as-vagrant.sh
+++ vagrant/provision/once-as-vagrant.sh
@@ -26,6 +26,7 @@ composer --no-progress --prefer-dist install
 
 info "Create bash-alias 'app' for vagrant user"
 echo 'alias app="cd /app"' | tee /home/vagrant/.bash_aliases
+echo 'alias phpunit="./vendor/bin/phpunit"' | tee /home/vagrant/.bash_aliases
 
 info "Enabling colorized prompt for guest console"
 sed -i "s/#force_color_prompt=yes/force_color_prompt=yes/" /home/vagrant/.bashrc
--- web/index.php
+++ web/index.php
@@ -6,4 +6,31 @@
  * Time: 12:37
  */
 
-echo 'Hello world';
+include __DIR__ . '/../vendor/autoload.php';
+
+use app\helpers\DbInit;
+use Illuminate\Container\Container;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
+use Illuminate\Routing\Router;
+use Illuminate\Routing\UrlGenerator;
+
+ini_set('display_errors',1);
+
+$db = new DbInit(require  __DIR__ . '/../config/db.php');
+
+$container = new Container;
+$container->singleton('app', Container::class);
+
+$request = Request::capture();
+$container->instance(Request::class, $request);
+
+$events = new Dispatcher($container);
+$router = new Router($events, $container);
+
+require_once __DIR__ . '/../config/routes.php';
+
+$redirect = new Redirector(new UrlGenerator($router->getRoutes(), $request));
+$response = $router->dispatch($request);
+$response->send();
