73737b82c6c7c0116337ae48112b21f87ca6044f
Boiko Sergii <helvisbox@gmail.com>
2019-02-01 23:54:59 +0200

    Added tests;

--- tests/Unit/http/controllers/products/ProductCrudControllerTest.php
+++ tests/Unit/http/controllers/products/ProductCrudControllerTest.php
@@ -10,9 +10,11 @@ namespace Tests\Unit\http\controllers\products;
 
 
 use app\http\controllers\products\ProductCrudController;
+use app\http\requests\products\ProductRequest;
 use app\models\products\Product;
 use app\models\products\ProductCategory;
 use app\models\products\ProductsCategories;
+use JeffOchoa\ValidatorFactory;
 use Tests\Fixtures\models\ProductsCategoriesFixture;
 use Tests\Helpers\traits\RequestGenerator;
 use Tests\TestCase;
@@ -106,6 +108,112 @@ class ProductCrudControllerTest extends TestCase
             [3],
             [4],
         ];
+    }
+
+    /**
+     * @throws \Exception
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     * @throws \app\exceptions\validations\RequestValidationException
+     */
+    public function testEdit()
+    {
+        $id = 1;
+        $newProductName = 'edited product name';
+        $newData = [
+            'id' => $id,
+            'name' => $newProductName,
+        ];
+
+        $oldData = [
+            'id' => $id,
+            'name' => 'product 1',
+        ];
+
+        $this->assertDatabaseHas(Product::TABLE_NAME, $oldData);
+        $this->assertDatabaseMissing(Product::TABLE_NAME, $newData);
+
+        $request = $this->genRequest($newData);
+        $controller = $this->makeController();
+        $factory = app()->make(ValidatorFactory::class);
+
+        $controller->edit($id, new ProductRequest($request, $factory));
+
+        $this->assertDatabaseMissing(Product::TABLE_NAME, $oldData);
+        $this->assertDatabaseHas(Product::TABLE_NAME, $newData);
+    }
+
+    /**
+     * @throws \Exception
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     * @throws \app\exceptions\validations\RequestValidationException
+     */
+    public function testEditWithDifferentCategoryIds()
+    {
+        $id = 1;
+        $newProductName = 'edited product name';
+        $newData = [
+            'id' => $id,
+            'name' => $newProductName,
+        ];
+
+        $newCategoryIds = ['categoryIds' => [2, 4]];
+
+        $oldData = [
+            'id' => $id,
+            'name' => 'product 1',
+        ];
+
+        $this->assertDatabaseHas(Product::TABLE_NAME, $oldData);
+        $this->assertDatabaseMissing(Product::TABLE_NAME, $newData);
+
+        $this->assertDatabaseHas(ProductsCategories::TABLE_NAME, [
+            'product_id' => $id,
+            'product_cat_id' => 1,
+        ]);
+
+        $this->assertDatabaseHas(ProductsCategories::TABLE_NAME, [
+            'product_id' => $id,
+            'product_cat_id' => 3,
+        ]);
+
+        $this->assertDatabaseMissing(ProductsCategories::TABLE_NAME, [
+            'product_id' => $id,
+            'product_cat_id' => 2,
+        ]);
+
+        $this->assertDatabaseMissing(ProductsCategories::TABLE_NAME, [
+            'product_id' => $id,
+            'product_cat_id' => 4,
+        ]);
+
+        $request = $this->genRequest(array_merge($newData, $newCategoryIds));
+        $controller = $this->makeController();
+        $factory = app()->make(ValidatorFactory::class);
+
+        $controller->edit($id, new ProductRequest($request, $factory));
+
+        $this->assertDatabaseMissing(Product::TABLE_NAME, $oldData);
+        $this->assertDatabaseHas(Product::TABLE_NAME, $newData);
+
+        $this->assertDatabaseMissing(ProductsCategories::TABLE_NAME, [
+            'product_id' => $id,
+            'product_cat_id' => 1,
+        ]);
+
+        $this->assertDatabaseMissing(ProductsCategories::TABLE_NAME, [
+            'product_id' => $id,
+            'product_cat_id' => 3,
+        ]);
+
+        $this->assertDatabaseHas(ProductsCategories::TABLE_NAME, [
+            'product_id' => $id,
+            'product_cat_id' => 2,
+        ]);
+
+        $this->assertDatabaseHas(ProductsCategories::TABLE_NAME, [
+            'product_id' => $id,
+            'product_cat_id' => 4,
+        ]);
     }
 
     /**
